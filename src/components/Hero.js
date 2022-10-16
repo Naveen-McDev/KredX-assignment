6efda0fbf27794675becf92027ee784848babb22
Naveen Krish <98112231+Naveen-McDev@users.noreply.github.com>
2022-10-16 11:28:22 +0530

    hero section without form

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,11 @@
 import React from "react";
-
+import heroImage from "../asset/heroImage.png";
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="hero__container">
+      <img src={heroImage} alt="HeroImage" className="hero__img" />
+    </div>
+  );
 };
 
 export default Hero;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,58 +3,56 @@ import brandLogo from "../asset/brandLogo.svg";
 
 const Navbar = () => {
   return (
-    <div>
-      <nav class="navbar navbar-expand-lg bg-light px-5">
-        <div class="container-fluid">
-          <a class="navbar-brand d-flex align-items-center" href="/">
-            <img src={brandLogo} alt="brand-logo" className="nav__logo" />
-          </a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div
-            class="collapse navbar-collapse justify-content-end"
-            id="navbarNav"
-          >
-            <ul class="navbar-nav mr-auto gap-3">
-              <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="/">
-                  Menu 1
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/">
-                  Menu 2
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/">
-                  Menu 3
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/">
-                  Menu 4
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/">
-                  Menu 5
-                </a>
-              </li>
-            </ul>
-          </div>
+    <nav class="navbar navbar-expand-lg bg-light px-5">
+      <div class="container-fluid">
+        <a class="navbar-brand d-flex align-items-center" href="/">
+          <img src={brandLogo} alt="brand-logo" className="nav__logo" />
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div
+          class="collapse navbar-collapse justify-content-end"
+          id="navbarNav"
+        >
+          <ul class="navbar-nav mr-auto gap-3">
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page" href="/">
+                Menu 1
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/">
+                Menu 2
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/">
+                Menu 3
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/">
+                Menu 4
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/">
+                Menu 5
+              </a>
+            </li>
+          </ul>
         </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
   );
 };
 
