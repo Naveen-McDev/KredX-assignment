72ee9084f202e8215779ad56903cdeaf61efb511
Naveen Krish <98112231+Naveen-McDev@users.noreply.github.com>
2022-10-16 09:43:57 +0530

    intial setup

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Hero from "../components/Hero";
+import Features from "../components/Features";
+import ContactUs from "../components/ContactUs";
+import Achievments from "../components/Achievments";
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <Achievments />
+      <ContactUs />
+    </>
+  );
+};
+
+export default Home;
